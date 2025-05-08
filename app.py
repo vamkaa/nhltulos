4355e0da476895a5b2ed2291bd6b919bfae6c7ec
vamkaa <valtteri.kaartosalmi@gmail.com>
2025-05-08 17:29:06 +0300

    Päivitetty käyttöliittymä: pelaajan haku

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 import streamlit as st
 
+#Valintasivu
 st.title("🏒 NHL Tulosveikkaaja")
+st.subheader("Valitse ennustettava kohde:")
 
-# Syötteet
-kotijoukkue = st.text_input("Kotijoukkueen nimi")
-vierasjoukkue = st.text_input("Vierasjoukkueen nimi")
-playoff = st.selectbox("Onko kyseessä playoff-peli?", ["Kyllä", "Ei"]) == "Kyllä"
+# Valitaan kumpaa ennustetaan
+valinta = st.selectbox("Mitä haluat ennustaa?", ["Ottelun voittaja", "Pelaajan maalin todennäköisyys"])
 
-# Ennustuspainike
-if st.button("Veikkaa voittaja"):
-    # Tässä kutsutaan myöhemmin muiden tekemää ennustefunktiota
-    st.write("Syötetyt tiedot:")
-    st.write(f"Kotijoukkue: {kotijoukkue}")
-    st.write(f"Vierasjoukkue: {vierasjoukkue}")
-    st.write(f"Onko playoff: {playoff}")
-    st.success("(Tähän tulee ennustetulos myöhemmin)")
+# Ennustelomake ottelun voittajalle
+if valinta == "Ottelun voittaja":
+    st.header("⚔️ Ennusta ottelun voittaja")
+    
+    # Syötteet
+    kotijoukkue = st.text_input("Kotijoukkueen nimi")
+    vierasjoukkue = st.text_input("Vierasjoukkueen nimi")
+    playoff = st.selectbox("Onko kyseessä playoff-peli?", ["Kyllä", "Ei"]) == "Kyllä"
+
+    # Ennustuspainike
+    if st.button("Veikkaa voittaja"):
+        st.write("Syötetyt tiedot:")
+        st.write(f"Kotijoukkue: {kotijoukkue}")
+        st.write(f"Vierasjoukkue: {vierasjoukkue}")
+        st.write(f"Onko playoff: {playoff}")
+        st.success("(Tähän tulee ottelun ennustetulos myöhemmin)")
+
+# Ennustelomake pelaajan maalille
+elif valinta == "Pelaajan maalin todennäköisyys":
+    st.header("🥅 Ennusta pelaajan maalin todennäköisyys")
+    
+    # Syöte: pelkkä pelaajan nimi
+    pelaaja = st.text_input("Pelaajan nimi")
+
+    # Ennustuspainike
+    if st.button("Laske maalin todennäköisyys"):
+        st.write("Syötetyt tiedot:")
+        st.write(f"Pelaaja: {pelaaja}")
+        st.success("(Tähän tulee maalin todennäköisyys myöhemmin)")
